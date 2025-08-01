8a9ab29a44ce558d541484811869317481f80d33
GulshenEzimli <gulshn.azimli@gmail.com>
2025-08-01 10:42:01 +0400

    Add all entities for project

---
+++ Core/Domain/Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Common.Abstraction;
+
+namespace Domain.Entities
+{
+	public class Product : BaseEntity
+	{
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public decimal Price { get; set; }
+		public decimal Discount { get; set; }
+		public int BrandId { get; set; }
+		public Brand Brand { get; set; }
+	}
+}
