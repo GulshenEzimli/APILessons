8086e7475a2e561ea0f3020200946aaf26e98c41
GulshenEzimli <gulshn.azimli@gmail.com>
2025-08-21 20:40:28 +0400

    Add controller to get all products

---
+++ Presentation/LessonsWebApi/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Application.Interfaces.UnitOfWorks;
+using Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LessonsWebApi.Controllers
+{
+	[Route("api/[controller]/[action]")]
+	[ApiController]
+	public class ProductController : ControllerBase
+	{
+		private readonly IUnitOfWork _unitOfWork;
+		public ProductController(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+		[HttpGet]
+		public async Task<IActionResult> GetAll()
+		{
+			var entities = await _unitOfWork.GetReadRepository<Product>().GetAllAsync();
+			return Ok(entities);
+		}
+
+	}
+}
