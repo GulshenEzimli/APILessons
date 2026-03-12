d17c9330ea3fef9bfe4eaee1d09947264707e996
GulshenEzimli <gulshn.azimli@gmail.com>
2026-03-12 09:51:25 +0400

    Add all entities and base interface to mark entities

---
+++ Core/Domain/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class Category : EntityBase
+    {
+        public int Name { get; set; }
+        public int Priority { get; set; }
+        public int ParentId { get; set; }
+        public Category Parent { get; set; }
+        public ICollection<Detail> Details { get; set; }
+        public ICollection<CategoryProduct> CategoryProducts { get; set; }
+
+    }
+}
