ba79ba476b4e9b90e2da3920ad6a94df076e7c77
GulshenEzimli <gulshn.azimli@gmail.com>
2026-03-15 16:07:14 +0400

    Create static registration class to add DbContext as service to web api project

---
+++ Infrastructure/Persistence/Registration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Context;
+
+namespace Persistence
+{
+    public static class Registration
+    {
+        public static void AddDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            string connectionstring = configuration.GetSection("ConnectionStrings:Default").Value;
+            services.AddDbContext<ApiLessonsDbContext>(options => options.UseSqlServer(connectionstring));
+
+        }
+    }
+}
