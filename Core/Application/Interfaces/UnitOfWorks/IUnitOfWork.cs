8b590660a44686b71a6b8a113a444d0d6ceb4029
GulshenEzimli <gulshn.azimli@gmail.com>
2026-03-24 12:40:17 +0400

    Add transaction methods for db operations in unitofwork

--- Core/Application/Interfaces/UnitOfWorks/IUnitOfWork.cs
+++ Core/Application/Interfaces/UnitOfWorks/IUnitOfWork.cs
@@ -7,7 +7,12 @@ namespace Application.Interfaces.UnitOfWorks
     public interface IUnitOfWork : IAsyncDisposable
     {
         IBaseRepository<T> BaseRepository<T>() where T : class, IEntityBase, new();
-        Task<int> SaveAsync();
+        Task<int> SaveAsync(CancellationToken cancellationToken = default);
         int Save();
+
+        Task BeginTransactionAsync(CancellationToken cancellationToken = default);
+        Task CommitTransactionAsync(CancellationToken cancellationToken = default);
+        Task RollbackTransactionAsync(CancellationToken cancellationToken = default);
+        Task DisposeTransactionAsync();
     }
 }
--- Infrastructure/Persistence/UnitOfWorks/UnitOfWork.cs
+++ Infrastructure/Persistence/UnitOfWorks/UnitOfWork.cs
@@ -1,5 +1,7 @@
 ﻿using Application.Interfaces.Repositories.EfCore;
 using Application.Interfaces.UnitOfWorks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
 using Persistence.Context;
 using Persistence.Repositories.EfCore;
 
@@ -8,17 +10,79 @@ namespace Persistence.UnitOfWorks
     public class UnitOfWork : IUnitOfWork
     {
         private readonly ApiLessonsDbContext _dbcontext;
+        private IDbContextTransaction? _dbTransaction;
+        private bool _disposed;
         public UnitOfWork(ApiLessonsDbContext context)
         {
             _dbcontext = context;
+            _disposed = true;
         }
 
-        IBaseRepository<T> IUnitOfWork.BaseRepository<T>() => new BaseRepository<T>(_dbcontext); 
+        #region Private
+        private async Task CreateTransactionIfNotExistsAsync(CancellationToken cancellationToken = default)
+        {
+            if (_dbTransaction == null)
+            {
+                _dbTransaction = await _dbcontext.Database.BeginTransactionAsync(cancellationToken);
+                _disposed = false;
+            }
+        }
+        #endregion
 
-        public async Task<int> SaveAsync() => await _dbcontext.SaveChangesAsync();
+        #region public methods
+        IBaseRepository<T> IUnitOfWork.BaseRepository<T>()
+        {
+            return new BaseRepository<T>(_dbcontext);
+        }
 
-        public int Save() => _dbcontext.SaveChanges();
+        public async Task<int> SaveAsync(CancellationToken cancellationToken = default)
+        {
+            return await _dbcontext.SaveChangesAsync(cancellationToken);
+        }
 
-        public async ValueTask DisposeAsync() => await _dbcontext.DisposeAsync();
+        public int Save()
+        {
+            return _dbcontext.SaveChanges();
+        }
+
+       
+        public async Task BeginTransactionAsync(CancellationToken cancellationToken = default)
+        {
+            await CreateTransactionIfNotExistsAsync(cancellationToken);
+        }
+
+        public async Task CommitTransactionAsync(CancellationToken cancellationToken = default)
+        {
+            if (_dbTransaction == null)
+                throw new InvalidOperationException("Aktiv tranzaksiya tapilmadi");
+
+            await _dbTransaction.CommitAsync(cancellationToken);
+        }
+
+        public async Task RollbackTransactionAsync(CancellationToken cancellationToken = default)
+        {
+            if (_dbTransaction == null)
+                throw new InvalidOperationException("Aktiv tranzaksiya tapilmadi");
+
+            await _dbTransaction.RollbackAsync(cancellationToken);
+        }
+
+        public async Task DisposeTransactionAsync()
+        {
+            if (_dbTransaction != null && !_disposed)
+            {
+                await _dbTransaction.DisposeAsync();
+                _dbTransaction = null;
+            }
+            _disposed = true;
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await DisposeTransactionAsync();
+            await _dbcontext.DisposeAsync();
+        }
+
+        #endregion
     }
 }
